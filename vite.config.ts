689a91961dfae3594cfc008e9144f4de42a571fd
Dany <zdanymc@gmail.com>
2023-06-01 15:48:52 +0200

    optimized build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { join } from 'path'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from "unplugin-icons/resolver"
@@ -10,51 +9,65 @@ import Unfonts from 'unplugin-fonts/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // when not using docker, it may be necessary to specify where the .env is
-  const env = loadEnv(mode, join(process.cwd(), '../'), '')
-
-  return {
-    plugins: [
-      vue(),
-      AutoImport({
-        dts: true,
-        imports: [
-          'vue',
-          'vue-router',
-          '@vueuse/core',
-          'pinia'
-        ],
-        eslintrc: {
-          enabled: true
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: true,
+      imports: [
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+        'pinia'
+      ],
+      eslintrc: {
+        enabled: true
+      }
+    }),
+    Components({
+      dts: true,
+      resolvers: [
+        HeadlessUiResolver({ prefix: "" }),
+        IconsResolver({ prefix: "" })
+      ]
+    }),
+    Icons({ autoInstall: true }),
+    Unfonts({
+      custom: {
+        families: [{
+          name: 'Ubuntu',
+          local: 'Ubuntu',
+          src: './src/assets/fonts/*.ttf'
+        }],
+        display: 'auto',
+        preload: true,
+        prefetch: false,
+      }
+    }),
+    tsconfigPaths()
+  ],
+  server: {
+    port: 3000,
+    open: false,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        minifyInternalExports: true,
+        entryFileNames: 'assets/cat.js',
+        assetFileNames: (info) => `assets/${info.name?.endsWith('css') ? 'cat' : '[name]'}[extname]`,
+        chunkFileNames: 'chunk.js',
+        manualChunks: () => 'chunk.js',
+        generatedCode: {
+          preset: 'es2015',
+          constBindings: true,
+          objectShorthand: true
         }
-      }),
-      Components({
-        dts: true,
-        resolvers: [
-          HeadlessUiResolver({ prefix: "" }),
-          IconsResolver({ prefix: "" })
-        ]
-      }),
-      Icons({ autoInstall: true }),
-      Unfonts({
-        custom: {
-          families: [{
-            name: 'Ubuntu',
-            local: 'Ubuntu',
-            src: './src/assets/fonts/*.ttf'
-          }],
-          display: 'auto',
-          preload: true,
-          prefetch: false,
-        }
-      }),
-      tsconfigPaths()
-    ],
-    server: {
-      port: parseInt(env.ADMIN_PORT ?? "3000"),
-      open: false,
-      host: true
+      }
     }
   }
 })
