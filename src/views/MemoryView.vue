31d4d2b9a734bef872e1252f91a569699afffedb
Dany <zdanymc@gmail.com>
2023-06-09 17:46:48 +0200

    removed tests

--- src/views/MemoryView.vue
+++ src/views/MemoryView.vue
@@ -3,14 +3,10 @@ import { useMemory } from '@stores/useMemory'
 import { useSettings } from '@stores/useSettings'
 import { JsonTreeView } from 'json-tree-view-vue3'
 import SelectBox from '@components/SelectBox.vue'
-import Plotly from '@aurium/vue-plotly'
-//import { TSNE } from '@keckelt/tsne'
-import { Matrix, TSNE, cosine } from "@saehrimnir/druidjs"
 
 const { isDark } = storeToRefs(useSettings())
 
 const callText = ref(''), callOutput = ref('{}'), kMems = ref(10)
-const outputAxisX = ref<number[]>([]), outputAxisY = ref<number[]>([]), outputTexts = ref<string[]>([])
 const selectCollection = ref<InstanceType<typeof SelectBox>>()
 
 const { wipeAllCollections, wipeCollection, callMemory } = useMemory()
@@ -34,30 +30,6 @@ const recallMemory = async () => {
 		callText.value = ' '
 	}
 	const result = await callMemory(callText.value, kMems.value)
-	const mat = result.episodic //Array.from({ length: 100 }).map(() => Array.from({ length: 1500 }).map(() => Math.random()))
-	//const druidMat = Matrix.from(mat.map(v => v.vector))
-	/*const scores = mat.map((v, i, k) => {
-		const len = k.length
-		return v.vector.map(j => {
-			const avg = v.vector.reduce((p, c) => p + c) / len
-			const dev = Math.sqrt(v.vector.map(x => Math.pow(x - avg, 2)).reduce((a, b) => a + b) / len)
-			return (j - avg) / dev
-		})
-	})*/
-	/*const tsne = new TSNE({
-		epsilon: Math.max(mat.length / 3, 50), // epsilon is learning rate (10 = default)
-		perplexity: 5, // roughly how many neighbors each point influences (30 = default)
-		dim: 2 // dimensionality of the embedding (2 = default)
-	})
-	tsne.initDataDist(mat.map(v => v.vector))
-	for(let k = 0; k < 1000; k++) {
-		tsne.step(); // every time you call this, solution gets better
-	}
-	const solution = tsne.getSolution() as number[][]*/
-	/*const druidTSNE = new TSNE(druidMat, { metric: cosine }).transform().to2dArray
-	outputAxisX.value = druidTSNE.map(v => v[0]) as number[]
-	outputAxisY.value = druidTSNE.map(v => v[1]) as number[]
-	outputTexts.value = result.episodic.map(v => v.page_content)*/
 	callOutput.value = JSON.stringify(result)
 }
 </script>
@@ -102,28 +74,6 @@ const recallMemory = async () => {
 			</div>
 		</div>
 		<div v-if="callOutput != '{}'" class="flex flex-wrap justify-center gap-4">
-			<!--<Plotly :data="[{
-				x: outputAxisX,
-				y: outputAxisY,
-				text: outputTexts,
-				type: 'scatter',
-				mode: 'markers',
-				name: 'Episodic',
-				marker: { size: 10 },
-			}]" :layout="{
-				title: 't-SNE',
-				font: {
-					family: 'Ubuntu',
-					size: 12,
-					color: isDark ? '#F4F4F5' : '#383938'
-				},
-				xaxis: { color: isDark ? '#F4F4F5' : '#383938' },
-				yaxis: { color: isDark ? '#F4F4F5' : '#383938' },
-				paper_bgcolor: isDark ? '#383938' : '#F4F4F5',
-				plot_bgcolor: isDark ? '#383938' : '#F4F4F5',
-				showlegend: false,
-				margin: { b: 40, l: 40, t: 40, r: 40 }
-			}" :displayModeBar="false" />-->
 			<JsonTreeView :data="callOutput" rootKey="result" :colorScheme="isDark ? 'dark' : 'light'" />
 		</div>
 	</div>
