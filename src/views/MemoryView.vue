1b6fd2fe4b165fbca11e05cdabdb42cecc318377
Dany <zdanymc@gmail.com>
2023-06-12 15:00:02 +0200

    Update MemoryView.vue

--- src/views/MemoryView.vue
+++ src/views/MemoryView.vue
@@ -45,8 +45,6 @@ const recallMemory = async () => {
 		kMems.value = 10
 	}
 
-	console.time("Tempo")
-
 	const result = await callMemory(callText.value, kMems.value)
 	callOutput.value = JSON.stringify(result)
 
@@ -60,8 +58,6 @@ const recallMemory = async () => {
 		.concat(queryMat, "vertical"), {
 		perplexity: Math.min(Math.max(kMems.value, 2), result.episodic.length + result.declarative.length)
 	}).transform(1000).asArray
-
-	console.timeEnd("Tempo")
 	
 	plotOutput.value = [
 		{
