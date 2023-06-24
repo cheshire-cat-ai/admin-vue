8301fc62f8c1fd5505394b028b2381ca5ef35206
lgazzola <luca.gazzola@lastminute.com>
2023-06-24 21:50:11 +0300

    removed title assertion

--- e2e/Example.spec.ts
+++ e2e/Example.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/')
-  await expect(page).toHaveTitle(/Ceshire Cat/)
   await page.getByText('Memory').click();
   await page.getByText('Plugins').click();
 })
