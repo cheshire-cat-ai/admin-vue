08c9d791587e94f5a09465ca8dab3c67fb6daea1
lgazzola <luca.gazzola@lastminute.com>
2023-06-24 21:44:59 +0300

    added test click

--- e2e/Example.spec.ts
+++ e2e/Example.spec.ts
@@ -5,4 +5,5 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/')
   await expect(page).toHaveTitle(/Ceshire Cat/)
   await page.getByText('Memory').click();
+  await page.getByText('Plugins').click();
 })
