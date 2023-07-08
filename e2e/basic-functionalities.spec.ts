8518c2f1a36320fd1fe18d1fcec8c3507239c451
lgazzola <luca.gazzola@lastminute.com>
2023-07-08 17:40:54 +0300

    using different locator

--- e2e/basic-functionalities.spec.ts
+++ e2e/basic-functionalities.spec.ts
@@ -2,12 +2,11 @@
 import { test, expect } from '@playwright/test'
 
 test('chat', async ({ page }) => {
-  var chatTextfield = "xpath=/html//div[@id='app']/div//textarea[@placeholder='Ask the Cheshire Cat...']"
   var sendButton = "xpath=/html//div[@id='app']/div/div[3]//div[@class='relative w-full']/div/button"
 
   await page.goto('http://localhost:3000')
   await page.getByText('Home').click()
-  await page.locator(chatTextfield).fill('who are you?')
+  await page.getByPlaceholder('Ask the Cheshire Cat...').fill('who are you?')
   await page.locator(sendButton).click()
   page.getByText('who are you?')
 })
