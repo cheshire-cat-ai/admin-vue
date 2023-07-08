4665775a4bf3c6995e9bf398074d857982ae055e
lgazzola <luca.gazzola@lastminute.com>
2023-07-08 14:58:18 +0300

    added assertions on menu pages

--- e2e/basic-functionalities.spec.ts
+++ e2e/basic-functionalities.spec.ts
@@ -4,6 +4,7 @@ import { test, expect } from '@playwright/test'
 test('chat', async ({ page }) => {
   var chatTextfield = "xpath=/html//div[@id='app']/div//textarea[@placeholder='Ask the Cheshire Cat...']"
   var sendButton = "xpath=/html//div[@id='app']/div/div[3]//div[@class='relative w-full']/div/button"
+
   await page.goto('http://localhost:3000')
   await page.getByText('Home').click()
   await page.locator(chatTextfield).fill('who are you?')
@@ -11,12 +12,27 @@ test('chat', async ({ page }) => {
   page.getByText('who are you?')
 })
 
-test('menu navigation', async ({ page }) => {
+test('menu navigation', async ({ page, context }) => {
   var settings = "xpath=/html//div[@id='app']//div[@class='navbar-center']/ul//a[@href='/settings']"
+  var micButton = "xpath=/html//div[@id='app']/div/div[3]/div[2]/div/button"
+
   await page.goto('http://localhost:3000')
+
   await page.getByText('Home').click()
+  page.locator(micButton)
+
   await page.getByText('Memory').click()
+  page.getByText('Recall text')
+
   await page.getByText('Plugins').click()
+  page.getByText('Search for a plugin')
+
   await page.locator(settings).click()
+  page.getByText('Set up your Cat')
+
+  const pagePromise = context.waitForEvent('page');
   await page.getByText('Docs').click()
+  const newPage = await pagePromise;
+  await newPage.waitForLoadState();
+  await newPage.waitForURL('https://cheshire-cat-ai.github.io/docs/')
 })
